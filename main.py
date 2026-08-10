a2cd6177f9e2d59a4e338466628ccc3024b44a23
Keysi_Proa00020 <79436814+KeysiKen@users.noreply.github.com>
2026-08-09 23:31:42 -0500

    Programa Principal

--- main.py
+++ main.py
@@ -8,12 +8,47 @@ if opcion == "1":
     kilometros = float(input("Ingrese kilometros: "))
     millas = kilometros * 0.621371
     print(f"{kilometros} Km equivale a {millas:.2f} millas.")
-
+#
+# MENU DE TEMPERATURA 
+# 
     def menu_temperatura():
         print("--Temperatura--")
         print("1. celsius a Fahrenheit")
         print("2. Celsius a Kelvin")
         print("0. Volver") 
         opcion = input("Sellecione una opcion:")
+        if opcion == "1":
+            celsius = float(input("Ingrese grados Celcius:"))
+            fahrenheut = (celcius *9/5)+32
+            print(f"{celcius} C equivale a {fahrenheit:.2f}F.")
+        elif opcion == "2":
+            celsius = float(input("Ingrese grados Celcius:")) 
+            kelvin = celcius + 273.15print(f"{celcius}Cequicale a {kelvin:.2f}K.")
+        elif opcion == "0":
+            return
+        else:
+            print("Opcion invalida.") 
+        #
+        # DESDE AQUI EMPIEZA EL MENU PRINCIPAL  
+        #
+        def main():
+            while True:
+                menu_principal()
+                opcion = input("Seleccione una opcion:")
+
+                if opcion == "1":
+                    menu_distancia()
+                elif opcion == "2":
+                    menu_masa()
+                    elif opcion == "3:"
+                    menu_temperatura()
+                    elif opcion == "0:"
+print (Hasta pronto.)
+break
 else:
-    print("Opcion invalida.")
+print("opcion invalida.")
+
+if_name_ == "_main_":
+if __name__ == '__main__':
+    main()
+    
